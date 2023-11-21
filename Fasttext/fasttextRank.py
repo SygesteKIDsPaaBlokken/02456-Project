23b9d9f20b6c0637ed53c7a4ec1f29335d386da2
Fabian Scott <toellerandet@gmail.com>
2023-11-21 12:40:38 +0100

    Scoring updated

--- Fasttext/fasttextRank.py
+++ Fasttext/fasttextRank.py
@@ -4,20 +4,33 @@ import fasttext.util
 from time import time
 from tqdm import tqdm
 from numpy.linalg import norm
+import nltk
+from nltk.corpus import stopwords
 
 
 def score(A, B):
-    return np.dot(A,B)/(norm(A, axis=1)*norm(B))
+    return np.dot(A, B) / (norm(A, axis=1) * norm(B))
+
+
+def ftPhrase(passage, ftModel):
+    temp = []
+    for word in passage.split(' '):
+        if word not in stop_words:
+            temp.append(ftModel[word])
+    return np.mean(temp, axis=0)
 
 
 if __name__ == '__main__':
+    nltk.download('stopwords')
+    stop_words = set(stopwords.words('english'))
     startQuery = 0
     stopQuery = 200
-    corpusStop = -1     # -1 for all
+    corpusStop = -1  # -1 for all
     t = time()
+
     fasttext.util.download_model('en', if_exists='ignore')  # English
     ft = fasttext.load_model('cc.en.300.bin')
-    fasttext.util.reduce_model(ft, 100)         # Reduce the dimensions to save RAM
+    fasttext.util.reduce_model(ft, 100)  # Reduce the dimensions to save RAM
     dfCorpus = pd.read_csv('collection.tsv', sep='\t')
     dfQueries = pd.read_csv('msmarco-test2019-queries.tsv', sep='\t')
 
@@ -28,13 +41,10 @@ if __name__ == '__main__':
     print(f'Loading data and model took {time() - t:.3f} seconds')  # ~60
     t = time()
     try:
-        ftCorpus = pd.read_csv(f'ftCorpus.csv', index_col=0).values
+        ftCorpus = pd.read_csv(f'ftCorpusStop.csv', index_col=0).values
     except FileNotFoundError:
-        ftCorpus = []
-        for passage in tqdm(corpus, desc='Creating corpus vectors'):
-            ftCorpus.append(np.mean([ft[word] for word in passage.split(' ')], axis=0))
-        ftCorpus = np.array(ftCorpus)
-        pd.DataFrame(ftCorpus).to_csv(f'ftCorpusNew.csv')
+        ftCorpus = np.array([ftPhrase(passage, ft) for passage in tqdm(corpus, desc='Creating corpus vectors')])
+        pd.DataFrame(ftCorpus).to_csv(f'ftCorpusStop.csv')
     print(f'Calculating all passage vectors took {time() - t:.3f} seconds')
 
     N = 1000
@@ -44,16 +54,17 @@ if __name__ == '__main__':
     i = 0
     for query in tqdm(queries, desc='Scoring'):
         qid = queryIDs[i]
-        ftQuery = ft[query]
+        ftQuery = ftPhrase(query, ft)
         scores = score(ftQuery, ftCorpus)
         scoresArgsort = (-1 * scores).argsort()[:N]
 
-        queryColumn[i*N:(i+1) * N] = qid
-        passageColumn[i*N:(i+1) * N] = scoresArgsort    #
-        scoreColumn[i*N:(i+1) * N] = scores[scoresArgsort]
+        queryColumn[i * N:(i + 1) * N] = qid
+        passageColumn[i * N:(i + 1) * N] = scoresArgsort  #
+        scoreColumn[i * N:(i + 1) * N] = scores[scoresArgsort]
         i += 1
 
     t = time()
-    out = pd.DataFrame(np.vstack((queryColumn, np.array(passageColumn), np.array(scoreColumn))).T, columns=['query', 'passage', 'score'])
-    out.to_csv(f'RankingResults_{startQuery}_{stopQuery}.csv', sep=',')
+    out = pd.DataFrame(np.vstack((queryColumn, np.array(passageColumn), np.array(scoreColumn))).T,
+                       columns=['query', 'passage', 'score'])
+    out.to_csv(f'RankingResults_Stop_{startQuery}_{stopQuery}.csv', sep=',')
     print(f'Saving data took {time() - t:.3f} seconds')  #
--- evaluation.py
+++ evaluation.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
+
 def ScoringEvaluation(dfEval, dfModel, topK, name):
     results = [[] for _ in topK]
+    resultsCount = [[] for _ in topK]
     resultsMax = [[] for _ in topK]
 
     for qid in tqdm(np.unique(dfEval['query'])):
@@ -15,18 +17,27 @@ def ScoringEvaluation(dfEval, dfModel, topK, name):
             # Could be a list comprehension:
             passages = dfModel[dfModel['query'] == qid][:k]['passage']
             score = 0
+            count = 0
             for passage in passages:
-                score += sum(evalRows[evalRows['passage'] == passage]['score'].values)
+                temp = sum(evalRows[evalRows['passage'] == passage]['score'].values)
+                score += temp
+                count += 0 < temp
             results[i].append(score)
+            resultsCount[i].append(count)
 
     print(name)
     sFT = ''
-    for k, resultMax, resultFT in zip(topK, resultsMax, results):
-        sFT += f'{k}: {sum(resultFT)}/{sum(resultMax)}\t'
+    for k, resultMax, resultFT, resultCount in zip(topK, resultsMax, results, resultsCount):
+        sFT += f'Score {k}: {sum(resultFT)}/{sum(resultMax)}\t'
+        sFT += f'Count {k}: {sum(resultCount)}/{k * len(np.unique(dfEval["query"]))}\n'
     print(sFT)
+    with open(name + '.txt', 'w') as f:
+        f.write(sFT)
+    return results
 
 
 def top1000Evaluation(dfTop, dfModel, topK, name):
+    # USELESS
     results = [[] for _ in topK]
     for qid in tqdm(np.unique(dfTop.values[:, 0])):
         # Find the relevant columns to speed up computation:
@@ -34,7 +45,7 @@ def top1000Evaluation(dfTop, dfModel, topK, name):
         for i, k in enumerate(topK):
             # Could be a list comprehension:
             passagesFT = dfModel[dfModel['query'] == qid][:k]['passage']
-
+            # Check for each passage whether it is there
             results[i].append(sum([sum(passage == evalRows[:, 1]) for passage in passagesFT]))
 
     print(f'{name} (average overlap)')
@@ -42,14 +53,17 @@ def top1000Evaluation(dfTop, dfModel, topK, name):
     for k, resultFT in zip(topK, results):
         sFT += f'{k}: {np.average(resultFT):.2}/{k}\t'
     print(sFT)
+    return results
 
 
 if __name__ == '__main__':
     dfEval = pd.read_csv('0-3scoringTestSet.txt', sep=' ')
     dfTop = pd.read_csv('msmarco-passagetest2019-top1000.tsv', sep='\t', header=None)
-    dfFT = pd.read_csv('Fasttext/RankingResults_0_200.csv', sep=',')
+    try:
+        dfFT = pd.read_csv('Fasttext/RankingResults_0_200.csv', sep=',')
+    except FileNotFoundError:
+        dfFT = pd.read_csv('RankingResults_0_200.csv', sep=',')
 
-    topK = [1, 5, 10, 100, 1000]
+    topK = [1, 5, 10, 100]
     ScoringEvaluation(dfEval, dfFT, topK, name='Fasttext')
     top1000Evaluation(dfTop, dfFT, topK, name='Fasttext')
-
