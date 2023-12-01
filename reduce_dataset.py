482d52f865dcc911306856083deea52e5538a4dc
Fabian Scott <toellerandet@gmail.com>
2023-12-01 16:16:52 +0100

    Update reduce_dataset.py
    Added filtering out queries

--- reduce_dataset.py
+++ reduce_dataset.py
@@ -1,11 +1,14 @@
 from time import time
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
 
 if __name__ == '__main__':
     N = 1_000_000
     t = time()
+    df_queries = pd.read_csv('queries.train.tsv', sep='\t', header=None)
+    # tripletsNew = pd.read_csv('tripletsReduced.tsv', sep='\t', header=None) # For convenience while creating the files
     dfTriplets = pd.read_csv('qidpidtriples.train.full.2.tsv', sep='\t', header=None)
     dfCollection = pd.read_csv('Fasttext/collection.tsv', sep='\t', header=None)
     dfScoring = pd.read_csv('0-3scoringTestSet.txt', sep=' ')
@@ -18,6 +21,14 @@ if __name__ == '__main__':
     N2 = len(collectNew)
     tripletsNew = dfTriplets.iloc[np.logical_or((dfTriplets.values[:, 1] < N2), (dfTriplets.values[:, 2] < N2))]
 
+    queries_new = []
+    unique_queries = np.unique(tripletsNew.values[:, 0])
+    for row in tqdm(df_queries.values[:, :]):
+        if sum(row[0] == unique_queries):
+            queries_new.append(row)
+
+    pd.DataFrame(np.array(queries_new).T).to_csv('queries.trainReduced.tsv', sep='\t', index=False, header=False)
     pd.DataFrame(collectNew).to_csv('Fasttext/collectionReduced.tsv', sep='\t', index=False, header=False)
     pd.DataFrame(tripletsNew).to_csv('tripletsReduced.tsv', sep='\t', index=False, header=False)
     print(f'Saving took {time() - t:.4} seconds')
+    pass
