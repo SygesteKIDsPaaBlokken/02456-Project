730572104c221a0d73ed224ae0d4d0f9f4e34e7d
Thomas Hegelund <02seifert@gmail.com>
2023-12-04 23:58:07 +0100

    Fixed callback and changed eval steps

--- utils/MSMarcoObjective.py
+++ utils/MSMarcoObjective.py
@@ -21,6 +21,7 @@ class MSMarcoObjective:
             batch_size=BATCH_SIZE,
             num_workers=NUM_WORKERS,
         )
+        self.limit = limit
 
         self.evaluator = make_evaluator()
 
@@ -32,16 +33,17 @@ class MSMarcoObjective:
         optimizer_name = trial.suggest_categorical("optimizer", ['AdamW', 'SGD'])
         optimizer_class = getattr(torch.optim, optimizer_name)
 
-        lr = trial.suggest_loguniform("lr", 1e-7, 1e-3)
+        lr = trial.suggest_float("lr", 1e-7, 1e-3, log=True)
 
         train_loss = losses.MultipleNegativesRankingLoss(model=model)
+        total_steps = self.limit // BATCH_SIZE
 
         model.fit(
             train_objectives=[(self.train_dataloader, train_loss)],
             epochs = 10,
             evaluator=self.evaluator,
-            evaluation_steps=100_000,
-            callback=trial.report,
+            evaluation_steps=0,
+            callback=lambda score,epoch,step: trial.report(score, epoch*total_steps + step),
             warmup_steps = WARMUP_STEPS,
             optimizer_class=optimizer_class,
             optimizer_params={'lr': lr},
