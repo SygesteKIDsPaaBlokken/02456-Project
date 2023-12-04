e22b7363ca1ae80dce936e95fcbd0bc90aaa8b8b
Thomas Hegelund <02seifert@gmail.com>
2023-12-05 00:51:02 +0100

    Fixed last step for post epoch eval

--- utils/MSMarcoObjective.py
+++ utils/MSMarcoObjective.py
@@ -36,14 +36,14 @@ class MSMarcoObjective:
         lr = trial.suggest_float("lr", 1e-7, 1e-3, log=True)
 
         train_loss = losses.MultipleNegativesRankingLoss(model=model)
-        total_steps = self.limit // BATCH_SIZE
+        n_steps_epoch = self.limit // BATCH_SIZE
 
         model.fit(
             train_objectives=[(self.train_dataloader, train_loss)],
             epochs = 10,
             evaluator=self.evaluator,
-            evaluation_steps=0,
-            callback=lambda score,epoch,step: trial.report(score, epoch*total_steps + step),
+            evaluation_steps=2000,
+            callback=lambda score,epoch,step: trial.report(evaluator_out_to_trial_report_in(n_steps_epoch, score, epoch, step)),
             warmup_steps = WARMUP_STEPS,
             optimizer_class=optimizer_class,
             optimizer_params={'lr': lr},
@@ -52,4 +52,15 @@ class MSMarcoObjective:
             output_path = 'trained_models/1epoch',
             save_best_model = SAVE_MODEL,
         )
+
+def evaluator_out_to_trial_report_in(n_steps_epoch: int, score: float, epoch: int, step: int) -> tuple[int, int]:
+    total_steps = 0
+
+    if step == -1:
+        total_steps = (epoch + 1) * n_steps_epoch
+    
+    else:
+        total_steps = epoch*n_steps_epoch + step
+    
+    return (score, total_steps)
     
