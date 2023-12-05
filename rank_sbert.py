d9c740e8d74da7a59b53d24362666168ec49c582
s214606 <s214606@student.dtu.dk>
2023-12-05 22:07:20 +0100

    Update SBERT ranking

--- rank_sbert.py
+++ rank_sbert.py
@@ -6,44 +6,39 @@ import nltk
 from nltk.corpus import stopwords
 from utils.evaluation import ScoringEvaluation
 from sentence_transformers import SentenceTransformer, util
-import torch
 from utils.config import DEVICE, DATA_FOLDER
 
 
-def rank_sbert(queries, queryIDs, passages, N=1000, max_l_dist=1, name='sbert'):
+def rank_sbert(queries, queryIDs, passages, N=1000, name='sbert'):
     MODEL_PATH = '1epoch'
     print("Using device:", DEVICE)
 
     model = SentenceTransformer(DATA_FOLDER + MODEL_PATH, device=DEVICE)
     
-    queryColumn, passageColumn, scoreColumn = [np.empty(len(queries) * N) for _ in range(3)]
-
-    # BEWARE BATCH SIZE
-
     print('#'*5, 'Encoding', '#'*5)
     SBERTCorpus = model.encode(passages, batch_size=32, show_progress_bar=True, device=DEVICE) 
     print('#'*5, 'Encoding finished', '#'*5)
 
     query_embeddings = model.encode(queries, batch_size=32, show_progress_bar=True, device=DEVICE) 
-    results = util.semantic_search(query_embeddings, SBERTCorpus, top_k=1000)
     
-    def create_dataframe(data):
-        df_list = []
-        query_id = 0
-
-        for query_results in tqdm(data, desc='Running through results'):
-            for result in query_results:
-                corpus_id = result['corpus_id']
-                score = result['score']
-                df_list.append({'query': queryIDs[query_id], 'passage': df_passages.values[corpus_id,0], 'score': score})
-            query_id += 1
-
-        df = pd.DataFrame(df_list).to_csv('data/SEMANTIC_SEARCH.csv')
+    ranking = util.semantic_search(query_embeddings, SBERTCorpus, top_k=1000)
+    
+    # Transform
+    df_list = []
+    query_id = 0
+    for query_results in tqdm(ranking, desc='Transforming'):
+        for result in query_results:
+            corpus_id = result['corpus_id']
+            score = result['score']
+            df_list.append({'query': queryIDs[query_id], 'passage': df_passages.values[corpus_id,0], 'score': score})
+        query_id += 1
         
-        return df
+    df = pd.DataFrame(df_list)
+    df.to_csv(f'data/{name}_RankingResults.csv')
+    
 
-    create_dataframe(results)
 
+    return df
     
 
     t = time()
@@ -78,7 +73,7 @@ if __name__ == '__main__':
     queries_clean = remove_stop_words(queries, stop_words)
     t = time()
 
-    df_sbert = rank_sbert(queries_clean, qids, df_passages.values[:, 1], max_l_dist=1, )
+    df_sbert = rank_sbert(queries_clean, qids, df_passages.values[:, 1], )
     #df_sbert = pd.read_csv('SEMANTIC_SEARCH.csv')
     print(f'SBERT searching took {(time() - t) / 60:.3f} minutes')  # ~
 
