0a09094179818a2725bb3add8c6087819168d25b
PBonvang <peter.bonvang@gmail.com>
2023-12-21 16:09:54 +0100

    bar labels added to MRR plot

--- plot_MRR.py
+++ plot_MRR.py
@@ -5,12 +5,14 @@ from utils.cb91visuals import *
 
 def plot_MRR(MRR_results:pd.DataFrame, save_path: str = None):
     plt.figure(figsize=(10, 5))
-    #plt.title('MRR@10 scores')
+    plt.title('MRR@10 scores', fontdict={'size':16})
     #plt.xlabel('MRR')
     #plt.ylabel('Model')
-    plt.tick_params('x',labeltop=True, labelbottom=False, bottom=False, top=False)
+    plt.tick_params('x',labeltop=False, labelbottom=True, bottom=False, top=False)
     plt.yticks(np.arange(len(MRR_results)), MRR_results['model'])
-    plt.barh(np.arange(len(MRR_results)), MRR_results['MRR']) 
+    bars = plt.barh(np.arange(len(MRR_results)), np.round(MRR_results['MRR'],3)) 
+    plt.bar_label(bars, label_type='edge', fontsize=10, padding=3)
+
     if save_path is not None:
         plt.savefig(save_path)
     plt.show()
@@ -18,7 +20,7 @@ def plot_MRR(MRR_results:pd.DataFrame, save_path: str = None):
 
 if __name__ == '__main__':
     df = pd.read_csv('data/evaluations/MRR.csv')
-    top100_models = df[df['model'].str.contains(r'^(?=.*top100)(?!.*top1000).*$')]
+    top100_models = df[df['model'].str.contains(r'^(?!.*Fuzzy_top)(?!.*top1000).*$')]
     print(top100_models)
     top100_models['model'] = top100_models['model'].str.replace('_top100.csv', '').str.replace('_', ' ')
     top100_models = top100_models.sort_values(by='MRR', ascending=True)
